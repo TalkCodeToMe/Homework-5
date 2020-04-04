e00052dc59fc082a101ad26ec431f66959f810dd
TalkCodeToMe <61719954+TalkCodeToMe@users.noreply.github.com>
2020-04-04 13:42:26 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+$('#currentDay').text(moment().format('MMMM Do YYYY, h:mm:ss a'))
+
+.text(div.col-2.hour)
+
+localStorage.setItem('row time block')
+
+var cat = localStorage.getItem('row time block')
+
+localStorage.removeItem('row time block');
+
+localStorage.clear('row time block');
+
